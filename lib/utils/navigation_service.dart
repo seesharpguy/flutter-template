a11a91ad72d8ccb69f26ba4a6faff8655af6fb87
Ryan Sites <ryan.sites@optum.com>
2020-12-16 10:26:56 -0500

    add arguments to navigator

--- lib/utils/navigation_service.dart
+++ lib/utils/navigation_service.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class NavigationService {
   final GlobalKey<NavigatorState> navigatorKey =
       new GlobalKey<NavigatorState>();
-  Future<dynamic> navigateTo(String routeName) {
-    return navigatorKey.currentState
-        .pushNamedAndRemoveUntil(routeName, (Route<dynamic> route) => false);
+
+  Future<dynamic> navigateTo(String routeName, {dynamic arguments}) {
+    return navigatorKey.currentState.pushNamedAndRemoveUntil(
+        routeName, (Route<dynamic> route) => false,
+        arguments: arguments);
   }
 
   void goBack() {
